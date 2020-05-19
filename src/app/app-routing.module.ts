5aa6f28682c9e976a8b6ae5a6619f136ac83a7c1
Baltazar9526 <32987728+Baltazar9526@users.noreply.github.com>
2020-05-18 19:26:12 -0500

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-const routes: Routes = [];
+import { InicioComponent } from './components/pages/inicio/inicio.component';
+import { GrafoBarraComponent } from './components/pages/grafo-barra/grafo-barra.component';
+import { GrafoPieComponent } from './components/pages/grafo-pie/grafo-pie.component';
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },{
+    path: 'grafoBarra',
+    component: GrafoBarraComponent
+  },{
+    path: 'grafoPie',
+    component: GrafoPieComponent
+  }
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/navigation/menu/menu.component';
-
+import { InicioComponent } from './components/pages/inicio/inicio.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { GrafoPieComponent } from './components/pages/grafo-pie/grafo-pie.component';
+import { GrafoBarraComponent } from './components/pages/grafo-barra/grafo-barra.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    InicioComponent,
+    GrafoPieComponent,
+    GrafoBarraComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule,
+    HttpClientModule,
+    NgSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
